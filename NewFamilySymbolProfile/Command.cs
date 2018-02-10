3539dcff7001915ee8696ee3099a22325108bb5b
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-02-10 16:05:07 +0100

    initial commit

---
+++ NewFamilySymbolProfile/Command.cs
@@ -0,0 +1,86 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+#endregion
+
+namespace NewFamilySymbolProfile
+{
+  [Transaction( TransactionMode.Manual )]
+  public class Command : IExternalCommand
+  {
+    const string _filepath = "C:/Users/All Users/Autodesk"
+      + "/RVT 2018/Libraries/UK/Profiles/Framing/Steel"
+      + "/Profiles_L-Angles.rfa";
+
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements )
+    {
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Application app = uiapp.Application;
+      Document doc = uidoc.Document;
+
+      Document profile_doc = app.OpenDocumentFile( 
+        _filepath );
+
+      // The profile document contains no family sybols:
+
+      FilteredElementCollector symbols
+        = new FilteredElementCollector( profile_doc )
+          .OfClass( typeof( FamilySymbol ) );
+
+      Debug.Assert( 0 == symbols.GetElementCount(), 
+        "expected no family symbol" );
+
+      // Load the profile family to generate them:
+
+      FamilySymbol profile_symbol = null;
+
+      using( Transaction tx = new Transaction( doc ) )
+      {
+        tx.Start( "Load Profile Family" );
+
+        Family family;
+
+        doc.LoadFamily( _filepath, out family );
+
+        tx.Commit();
+
+        ISet<ElementId> ids = family.GetFamilySymbolIds();
+
+        foreach( ElementId id in ids )
+        {
+          profile_symbol = doc.GetElement( id ) 
+            as FamilySymbol;
+
+          Debug.Print( profile_symbol.Name );
+        }
+      }
+
+      // Generate the family symbol profile:
+
+      FamilySymbolProfile fsp = null;
+
+      if( null != profile_symbol )
+      {
+        using( Transaction tx = new Transaction( doc ) )
+        {
+          tx.Start( "Create Sweep" );
+          fsp = app.Create.NewFamilySymbolProfile( 
+            profile_symbol );
+          tx.Commit();
+        }
+      }
+
+      return Result.Succeeded;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # NewFamilySymbolProfile
-C# .NET Revit API add-in to demonstrate the use of NewFamilySymbolProfile
+
+C# .NET Revit API add-in to demonstrate the use
+of [NewFamilySymbolProfile](http://www.revitapidocs.com/2018.1/0fd22223-8ca8-2955-e2ee-7d83d853ee8f.htm).
+
+Created for Dale Bartlett to answer his question
+on [sweep profile from family](https://forums.autodesk.com/t5/revit-api-forum/sweep-profile-from-family/m-p/7758601).
+
+Also illustrates further the answer provided in the previous thread
+on [sweep using profile family](https://forums.autodesk.com/t5/revit-api-forum/sweep-using-profile-family/m-p/6057115).
+
+
+## Author
+
+Jeremy Tammik,
+[The Building Coder](http://thebuildingcoder.typepad.com) and
+[The 3D Web Coder](http://the3dwebcoder.typepad.com),
+[Forge](http://forge.autodesk.com) [Platform](https://developer.autodesk.com) Development,
+[ADN](http://www.autodesk.com/adn)
+[Open](http://www.autodesk.com/adnopen),
+[Autodesk Inc.](http://www.autodesk.com)
+
+
+## License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+Please see the [LICENSE](LICENSE) file for full details.
